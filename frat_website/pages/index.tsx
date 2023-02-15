1e688c714a23e875705bf73cd4c8fc39a47a6236
AhmirPostell <124746123+AhmirPostell@users.noreply.github.com>
2023-02-15 12:09:22 -0500

    Finished header with linked icons

---
+++ frat_website/pages/index.tsx
@@ -0,0 +1,30 @@
+import Header from '../components/header';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Frat Website</title>
+      </Head>
+      
+        {/* Header */}
+        <Header />
+
+        {/* Hero */}
+
+        {/* About */}
+
+        {/* Experience */}
+
+        {/* Skills */}
+
+        {/* Projects */}
+
+        {/* Contact Us */}
+    
+    </>
+  )
+}
